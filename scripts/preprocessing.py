781f011bc0ffe2fd188e8fd5c176c76103ff426e
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-15 23:27:12 +0330

    Add datetime processing and feature extraction to DataFrame

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,8 +1,18 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_timestamp, hour, dayofweek, month
 
 spark = SparkSession.builder.appName("ElectricityPreprocessing").getOrCreate()
 
-df = spark.read.csv("D:\Programming\Data_Engineering\Apache_Spark\project\power_consumption_spark\Data\power.csv", header=True, inferSchema=True)
+df = spark.read.csv(r"D:\Programming\Data_Engineering\Apache_Spark\project\power_consumption_spark\Data\power.csv", header=True, inferSchema=True)
 
 print(df.printSchema())
 print(df.show(5))
+
+
+df = df.withColumn("datetime", to_timestamp("datetime", "yyyy-MM-dd HH:mm:ss"))
+
+df = df.withColumn("hour", hour("datetime"))
+df = df.withColumn("day_of_week", dayofweek("datetime"))
+df = df.withColumn("month", month("datetime"))
+
+
