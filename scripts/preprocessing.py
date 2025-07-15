c64b16f2ed24451951e3868cc4ee9ab85fe0cd1b
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-15 22:12:23 +0330

    Test Spark setup with basic PySpark job

---
+++ scripts/preprocessing.py
@@ -0,0 +1,10 @@
+import os
+os.environ["PYSPARK_PYTHON"] = r"D:\Programming\Data_Engineering\Apache_Spark\spark-env\Scripts\python.exe"
+os.environ["PYSPARK_DRIVER_PYTHON"] = r"D:\Programming\Data_Engineering\Apache_Spark\spark-env\Scripts\python.exe"
+
+from pyspark import SparkContext ,SparkConf 
+
+conf = SparkConf().setAppName("MyApp").setMaster("local")
+sc = SparkContext(conf=conf)
+
+
