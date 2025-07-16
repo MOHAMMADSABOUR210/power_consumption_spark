3293e412211c66f73d6ccd8da621a04f840d901d
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-16 18:14:09 +0330

    cheng ',' to ';' and fix errors

--- scripts/txt_to_csv_converter.py
+++ scripts/txt_to_csv_converter.py
@@ -1,17 +1,26 @@
 import csv
 
 input_file = '../Data/electricityloaddiagrams20112014/LD2011_2014.txt'  
-output_file = '../Data/power.csv'
+# output_file = '../Data/power.csv'
 
-with open(input_file, 'r', encoding='utf-8') as txtfile, \
-     open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+# input_file = 'input.txt'
+output_file = '../Data/output.txt'
 
-    txtfile = txtfile.read()
-    txtfile = txtfile.replace(',', ';')
+with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
+    for line in infile:
+        updated_line = line.replace(',', ';')
+        outfile.write(updated_line)
 
 
-    txt_reader = csv.reader(txtfile, delimiter=';')  
-    csv_writer = csv.writer(csvfile)
+# with open(input_file, 'r', encoding='utf-8') as txtfile, \
+#      open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
 
-    for row in txt_reader:
-        csv_writer.writerow(row)
+#     txtfile = txtfile.read()
+#     txtfile = txtfile.replace(',', ';')
+
+
+#     txt_reader = csv.reader(txtfile, delimiter=';')  
+#     csv_writer = csv.writer(csvfile)
+
+#     for row in txt_reader:
+#         csv_writer.writerow(row)
