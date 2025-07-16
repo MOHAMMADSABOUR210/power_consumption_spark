ccbd144e183ad559190d663d7e6dbca363c39ab0
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-16 12:38:58 +0330

    convert ',' to ';' in dataset txt

--- scripts/txt_to_csv_converter.py
+++ scripts/txt_to_csv_converter.py
@@ -1,12 +1,16 @@
 import csv
 
-input_file = '../Data/electricityloaddiagrams20112014/LD2011_2014.txt'   # Replace with your actual txt file name
-output_file = '../Data/power.csv' # Output CSV file name
+input_file = '../Data/electricityloaddiagrams20112014/LD2011_2014.txt'  
+output_file = '../Data/power.csv'
 
 with open(input_file, 'r', encoding='utf-8') as txtfile, \
      open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
-    
-    txt_reader = csv.reader(txtfile, delimiter=';')  # Use semicolon as delimiter
+
+    txtfile = txtfile.read()
+    txtfile = txtfile.replace(',', ';')
+
+
+    txt_reader = csv.reader(txtfile, delimiter=';')  
     csv_writer = csv.writer(csvfile)
 
     for row in txt_reader:
