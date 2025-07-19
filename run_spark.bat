b8f4efcb7098d4a0cf700d9b027d7f9ab83bfe3b
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-19 09:41:23 +0330

    Update run_spark.bat to include Hadoop environment variables (HADOOP_HOME and PATH)

--- run_spark.bat
+++ run_spark.bat
@@ -1,14 +1,17 @@
 @echo off
 
-REM 
+REM Activate virtual environment
 call D:\Programming\Data_Engineering\Apache_Spark\project\venv\Scripts\activate.bat
 
-REM 
+REM Set PySpark Python environment
 set PYSPARK_PYTHON=D:\Programming\Data_Engineering\Apache_Spark\project\venv\Scripts\python.exe
 set PYSPARK_DRIVER_PYTHON=D:\Programming\Data_Engineering\Apache_Spark\project\venv\Scripts\python.exe
 
-REM 
-%PYSPARK_PYTHON% scripts/preprocessing.py
+REM Set Hadoop environment
+set HADOOP_HOME=C:\hadoop\hadoop-3.3.6
+set PATH=%HADOOP_HOME%\bin;%PATH%
 
+REM Run your PySpark script
+%PYSPARK_PYTHON% scripts/preprocessing.py
 
 pause
