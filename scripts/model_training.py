ab0cb8c5100432534cfce1f5aaa4c0623bc703c0
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-22 17:22:48 +0330

    Code porting and optimization to build better and more efficient pipelines

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -12,10 +12,14 @@ spark = SparkSession.builder.appName("PowerPredictionModel").getOrCreate()
 data = spark.read.parquet(r"Data\featured_data")
 
 
-feature_cols = ['scaled_features']
-assembler = VectorAssembler(inputCols=feature_cols, outputCol="features")
-data = assembler.transform(data)
+numeric_cols = ['HDD18_3', 'CDD0', 'CDD10', 'PRECTOT', 'RH2M', 'T2M', 'T2M_MIN', 'T2M_MAX', 'ALLSKY']
 
+assembler = VectorAssembler(inputCols=numeric_cols, outputCol="features_vec")
+scaler = MinMaxScaler(inputCol="features_vec", outputCol="scaled_features")
+final_assembler = VectorAssembler(inputCols=["scaled_features"], outputCol="final_features")
+lr = LinearRegression(featuresCol="final_features", labelCol="ENERGY")
+
+pipeline = Pipeline(stages=[assembler, scaler, final_assembler, lr])
 
 train_data, test_data = data.randomSplit([0.8, 0.2], seed=42)
 
@@ -24,7 +28,8 @@ print("Train count:", train_data.count())
 print("Test count:", test_data.count())
 
 lr = LinearRegression(featuresCol="features", labelCol="ENERGY")
-lr_model = lr.fit(train_data)
+lr_model = pipeline.fit(train_data)
+
 
 test_predictions = lr_model.transform(test_data)
 
@@ -50,10 +55,7 @@ lr_model.save(model_path)
 
 #################################################prediction season ##############################################
 
-numeric_cols = ['HDD18_3', 'CDD0', 'CDD10', 'PRECTOT', 'RH2M', 'T2M', 'T2M_MIN', 'T2M_MAX', 'ALLSKY']
 
-assembler = VectorAssembler(inputCols=numeric_cols, outputCol="features_vec")
-scaler = MinMaxScaler(inputCol="features_vec", outputCol="scaled_features")
 season_indexer = StringIndexer(inputCol="season", outputCol="season_index")
 final_assembler = VectorAssembler(inputCols=["scaled_features", "season_index"], outputCol="final_features")
 lr = LinearRegression(featuresCol="final_features", labelCol="ENERGY")
@@ -80,3 +82,16 @@ print(f"Root Mean Squared Error (RMSE): {rmse}")
 now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 model_path = fr"D:\Programming\Data_Engineering\Apache_Spark\project\power_consumption_spark\Models\Model_Spark_{now}"
 model.save(model_path)
+
+######################################Predicte New Rows###########################################
+
+# from pyspark.sql import Row
+
+# new_data = [
+#     Row(HDD18_3=0.0, CDD0=10.0, CDD10=5.0, PRECTOT=1.2, RH2M=45.0,
+#         T2M=30.0, T2M_MIN=24.0, T2M_MAX=36.0, ALLSKY=0.3, season="summer")
+# ]
+# new_df = spark.createDataFrame(new_data)
+
+# prediction_new = model.transform(new_df)
+# prediction_new.select("season", "prediction").show()
