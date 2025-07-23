aadeb70d831e55e40d64007d3118557d08edc53b
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-23 22:15:39 +0330

    Generate synthetic power consumption data using sampling and controlled noise injection

--- scripts/predict_power_consumption.py
+++ scripts/predict_power_consumption.py
@@ -1,15 +1,19 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import GBTRegressionModel
+from pyspark.ml import PipelineModel
+import random
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import DoubleType
 
-# ساخت SparkSession
 spark = SparkSession.builder.appName("GBTModelPrediction").getOrCreate()
 
 model_path = "D:\Programming\Data_Engineering\Apache_Spark\project\power_consumption_spark\Models\Model_Spark_v1"
 
-model = GBTRegressionModel.load(model_path)
+model = PipelineModel.load(model_path)
 
-print(model.featuresCol)
+for i, stage in enumerate(model.stages):
+    print(f"Stage {i}: {stage.__class__.__name__}")
 
 numeric_cols = [
     'HDD18_3', 'CDD0', 'CDD10', 'PRECTOT', 'RH2M',
@@ -20,12 +24,30 @@ numeric_cols = [
     'energy_ma_3', 'energy_std_3'
 ]
 
-df_new = spark.createDataFrame(new_data, numeric_cols)
+data = spark.read.parquet(r"Data\featured_data")
 
-assembler = VectorAssembler(inputCols=numeric_cols, outputCol="features")
-df_new_vec = assembler.transform(df_new)
-
-predictions = model.transform(df_new_vec)
+df_sample = data.sample(withReplacement=False, fraction=0.05, seed=42)
 
 
-predictions.select(*numeric_cols, "prediction").show()
+def add_noise(value, intensity=0.10):
+    if value is None:
+        return None
+    noise = random.gauss(0, abs(value) * intensity)
+    return round(value + noise, 2)
+
+add_noise_udf = udf(lambda x: add_noise(x, intensity=0.05), DoubleType())
+
+for col_name in numeric_cols:
+    df_sample = df_sample.withColumn(col_name, add_noise_udf(col(col_name)))
+
+print(df_sample.show(10))
+
+# df_new = spark.createDataFrame(new_data, numeric_cols)
+
+# assembler = VectorAssembler(inputCols=numeric_cols, outputCol="features")
+# df_new_vec = assembler.transform(df_new)
+
+# predictions = model.transform(df_new_vec)
+
+
+# predictions.select(*numeric_cols, "prediction").show()
