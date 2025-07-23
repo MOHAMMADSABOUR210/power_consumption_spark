808043ff102e39f6f2e8867284542c6b639651fa
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-07-23 22:49:33 +0330

    Code optimization , Add model V2

--- scripts/predict_power_consumption.py
+++ scripts/predict_power_consumption.py
@@ -23,28 +23,45 @@ numeric_cols = [
 
 data = spark.read.parquet(r"Data\featured_data")
 
-df_sample = data.sample(withReplacement=False, fraction=0.05, seed=42)
 
+def data_sample(data,numeric_cols):
+    df_sample = data.sample(withReplacement=False, fraction=0.05, seed=42)
 
-def add_noise(value, intensity=0.10):
-    if value is None:
-        return None
-    noise = random.gauss(0, abs(value) * intensity)
-    return round(value + noise, 2)
+    def add_noise(value, intensity=0.10):
+        if value is None:
+            return None
+        noise = random.gauss(0, abs(value) * intensity)
+        return round(value + noise, 2)
 
-add_noise_udf = udf(lambda x: add_noise(x, intensity=0.05), DoubleType())
+    add_noise_udf = udf(lambda x: add_noise(x, intensity=0.05), DoubleType())
 
-df_sample = df_sample.select(numeric_cols)
+    df_sample = df_sample.select(numeric_cols)
 
-for col_name in numeric_cols:
-    df_sample = df_sample.withColumn(col_name, add_noise_udf(col(col_name)))
+    for col_name in numeric_cols:
+        df_sample = df_sample.withColumn(col_name, add_noise_udf(col(col_name)))
 
-print(df_sample.show(10))
+    print(df_sample.show(10))
 
+    return df_sample
+##################################### model v1 #################################
 
+df_sample = data_sample(data,numeric_cols)
 predictions = model.transform(df_sample)
 
 print(predictions.select("prediction").show(truncate=False))
 
 
+
+##################################### model v2 #################################
+
+model_path = "D:\Programming\Data_Engineering\Apache_Spark\project\power_consumption_spark\Models\Model_Spark_v2"
+
+model = PipelineModel.load(model_path)
+numeric_cols = numeric_cols + ["season"]
+
+
+df_sample = data_sample(data,numeric_cols)
+predictions = model.transform(df_sample)
+
+print(predictions.select("prediction").show(truncate=False))
 
